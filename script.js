708f2bb564cf9bde778bc58d120bfde49dd47699
ricardotramiro <ricardoteixeiraramiro@gmail.com>
2025-08-09 16:30:10 -0300

    Ajustes no formulário

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-document.getElementById('form-whatsapp').addEventListener('submit', function(e){
+document.getElementById('whatsapp-form').addEventListener('submit', function(e){
 e.preventDefault(); // Evita recarregar a página
 
 let name = document.getElementById('name').value.trim();
 let message = document.getElementById('message').value.trim();
-let button = this.querySelector('button');
 
 // Validação simples
 if (!name || !message) {
-    alert('por favor, preecha seu nome e a mensagem.');
+    alert('Por favor, preencha todos os campos.');
     return;
 }
 
-// Feedback no botão
-button.textContent = "Enviando...";
-button.disabled = true;
-
 // Monta o link do WhatsApp
 let url = `https://wa.me/5511962307325?text=Olá, meu nome é ${encodeURIComponent(name)}.
-. ${encodeURIComponent(message)}`;
+%0A${encodeURIComponent(message)}`;
 
 // Abre o Whatsapp em nova aba
 window.open(url,'_blank');
 
-// Substitui o formulário pelo aviso de confirmação
-setTimeout(() => {
-    document.getElementById('form-whatsapp').style.display = 'none';
-   
-    let thankYouMessage = document.createElement('div');
-    thankYouMessage.classList.add('thank-you-message');
-    thankYouMessage.innerHTML = '✅ Sua mensagem foi enviada! Obrigado pelo contato.';
+// Esconde o formulário e mostra a mensagem de recebimento 
+document.getElementById('whatsapp-form').style.display = 'none';
+document.getElementById('form-title').style.display = 'none';
 
-    document.getElementById('contacts').appendChild(thankYouMessage);
-}, 800);
-})
+// Moatra a mensagem de confirmação
+document.getElementById('success-message').style.display = 'block';
+
+});
